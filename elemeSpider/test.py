9f1af93c2d1f65bbf5090df9bf6a2b2d9ae2fe1f
吴一发 <yifan.wu@qq.com>
2019-03-08 18:34:41 +0800

    测试eleme经纬度加密的方式

--- elemeSpider/test.py
+++ elemeSpider/test.py
@@ -1,10 +1,12 @@
-import execjs
+
+import js2py
 
 with open('./test.js', 'r', encoding='utf-8')as f:
     js_str = f.read()
 
-ctx = execjs.compile(js_str)
+a = js2py.eval_js(js_str)
+# print(a.encode(31.185375,121.611191))
+print(a.encode(25.087640, 104.934097))
 # ctx = execjs.compile(
-#     "var windows = 'ss' " 
+#     "var windows = 'ss' "
 # )
-print(ctx.call("aa()"))
